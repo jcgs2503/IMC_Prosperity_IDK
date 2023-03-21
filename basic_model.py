16b7dc2782079020a1cc8da6c5d05d5a1ac33cf9
Jerry Chang <jcgs2503@gmail.com>
2023-03-21 15:12:20 -0500

    updated strategy and data analysis

--- basic_model.py
+++ basic_model.py
@@ -1,8 +1,36 @@
 from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
+from datamodel import Order, ProsperityEncoder, TradingState, Symbol, OrderDepth
 import math
+import json
+import numpy as np
 
 # https://bz97lt8b1e.execute-api.eu-west-1.amazonaws.com/prod/results/tutorial/<algorithm id>
+
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: Dict[Symbol, List[Order]]) -> None:
+        logs = self.logs
+        if logs.endswith("\n"):
+            logs = logs[:-1]
+
+        print(json.dumps({
+            "state": state,
+            "orders": orders,
+            "logs": logs,
+        }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True))
+
+        self.state = None
+        self.orders = {}
+        self.logs = ""
+
+
+logger = Logger()
 
 
 class Trader:
@@ -24,6 +52,7 @@ class Trader:
                 acceptable_price = 10_000
                 buy_sum = 0
                 sell_sum = 0
+
                 if len(order_depth.sell_orders) > 0:
                     asks = list(order_depth.sell_orders.keys())
                     asks.sort()
@@ -49,14 +78,14 @@ class Trader:
                                     raise RuntimeError()
                                 sell_sum = sell_sum + volume
                                 orders.append(Order(product, bid, volume))
-                spread = 3
+                spread = 2
 
                 if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0:
                     asks = list(order_depth.sell_orders.keys())
                     asks.sort()
                     bids = list(order_depth.buy_orders.keys())
                     bids.sort(reverse=True)
-                    spread = (asks[0]-bids[0])/2
+                    spread = abs(asks[0]-bids[0])/10
 
                 if buy_sum + curr_pos < LIMIT:
                     orders.append(
@@ -65,40 +94,59 @@ class Trader:
                     orders.append(
                         Order(product, acceptable_price+spread/2, -sell_sum-LIMIT-curr_pos))
                 result[product] = orders
-            # if product == 'BANANAS':
-            #     LIMIT = 20
-            #     order_depth: OrderDepth = state.order_depths[product]
-            #     orders: list[Order] = []
+            if product == 'BANANAS':
+                LIMIT = 20
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: list[Order] = []
 
-            #     order_bias = sum(order_depth.sell_orders) + \
-            #         sum(order_depth.buy_orders)
-            #     if order_bias > 0:
-            #         asks = list(order_depth.sell_orders.keys())
-            #         asks.sort()
-            #         for ask in asks:
-            #             volume = -order_depth.sell_orders[ask]
-            #             if volume + curr_pos > LIMIT:
-            #                 volume = LIMIT - curr_pos
-            #             if volume != 0:
-            #                 orders.append(Order(product, ask, volume))
-            #     elif order_bias < 0:
-            #         bids = list(order_depth.buy_orders.keys())
-            #         bids.sort(reverse=True)
-            #         for bid in bids:
-            #             volume = -order_depth.buy_orders[bid]
-            #             if volume + curr_pos < -LIMIT:
-            #                 volume = -LIMIT - curr_pos
-            #             if volume != 0:
-            #                 orders.append(Order(product, bid, volume))
-            #     else:
-            #         mid_price = (min(order_depth.sell_orders.keys()) +
-            #                      max(order_depth.buy_orders.keys()))/2.0
-            #         orders.append(Order(product, mid_price, -curr_pos))
+                k1 = 2  # default spread size
 
-            #     result[product] = orders
+                q_s = sum(order_depth.sell_orders.values())
+                q_b = sum(order_depth.buy_orders.values())
+
+                avg_s = sum([order_depth.sell_orders[key] *
+                            key for key in order_depth.sell_orders.keys()])/sum(order_depth.sell_orders.values())
+                avg_b = sum([order_depth.buy_orders[key] *
+                            key for key in order_depth.buy_orders.keys()])/sum(order_depth.buy_orders.values())
+
+                # average bid/ask method
+                p_t = (avg_s+avg_b)/2
+
+                # price_sum = 0
+                # qt = 0
+                # if product in state.market_trades.keys():
+                #     for trade in state.market_trades[product]:
+                #         price_sum += trade.quantity*trade.price
+                #         qt += trade.quantity
+                #     if qt != 0 and qt > 7:
+                #         p_t = price_sum/qt
+
+                # Excessive buy
+                # if q_b + q_s == 0:
+                #     bid_price = p_t - k1/2
+                #     ask_price = p_t + k1/2
+
+                # elif (q_s-q_b)/(q_b+q_s) > 0.5:
+                #     bid_price = p_t
+                #     ask_price = p_t + k1
+
+                # # Excessive sell
+                # elif (q_b-q_s)/(q_b+q_s) > 0.5:
+                #     bid_price = p_t - k1
+                #     ask_price = p_t
+                # # Passive
+                # else:
+                #     bid_price = p_t - k1/2
+                #     ask_price = p_t + k1/2
+
+                orders.append(Order(product, bid_price, LIMIT - curr_pos))
+                orders.append(Order(product, ask_price, -curr_pos - LIMIT))
+
+                result[product] = orders
         # self.__filter(state, result)
         # self.__print_result(result)
         # self.__print_own_trades(state.own_trades)
+        logger.flush(state, result)
         return result
 
     def __filter(self, state: TradingState, result: Dict[str, List[Order]], limit=20):
